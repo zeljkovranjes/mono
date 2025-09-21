8ed5894c66f0f1f9e8388e437b26b86088f01028
chomnr <hello@zeljko.me>
2025-09-21 18:26:08 -0400

    added .env.example

--- apps/dashboard/src/routes/about.tsx
+++ apps/dashboard/src/routes/about.tsx
@@ -4,7 +4,9 @@ import Counter from '~/components/Counter';
 export default function About() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">About Page</h1>
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        {import.meta.env.VITE_ROOT_DOMAIN}
+      </h1>
       <Counter />
       <p class="mt-8">
         Visit{' '}
