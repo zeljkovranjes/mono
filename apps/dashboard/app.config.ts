a30315e09441020a1dacae2030c413e1a214eed3
chomnr <hello@zeljko.me>
2025-09-21 17:58:29 -0400

    removed baseurl

--- apps/dashboard/app.config.ts
+++ apps/dashboard/app.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from '@solidjs/start/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  server: {
-    baseURL: '/dashboard',
-  },
   vite: {
     plugins: [tailwindcss()],
   },
