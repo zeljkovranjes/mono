fe0d369038e5194fb6c3436d755859b7901a02dc
chomnr <hello@zeljko.me>
2025-09-22 18:27:04 -0400

    simple login page

--- apps/dashboard/src/routes/(auth)/login.tsx
+++ apps/dashboard/src/routes/(auth)/login.tsx
@@ -1,9 +1,38 @@
 import { Button } from '@safeoutput/ui/components/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@safeoutput/ui/components/card';
+import { LabeledSeparator, Separator } from '@safeoutput/ui/components/separator';
+import { TextField, TextFieldInput, TextFieldLabel } from '@safeoutput/ui/components/text-field';
 
 export default function Login() {
   return (
-    <>
-      <Button>Log in to your account</Button>
-    </>
+    <Card class="w-[384px] max-w-[384px]">
+      <CardHeader>
+        <CardTitle>Log in</CardTitle>
+        <CardDescription class="flex gap-1 items-center">
+          New to Safeoutput?
+          <Button variant="link" class="p-0">
+            Sign up
+          </Button>
+        </CardDescription>
+      </CardHeader>
+      <CardContent class="flex flex-col gap-5">
+        <LabeledSeparator>OR</LabeledSeparator>
+        <TextField>
+          <TextFieldLabel>Email</TextFieldLabel>
+          <TextFieldInput type="email" placeholder="example@domain.com" />
+        </TextField>
+        <TextField>
+          <TextFieldLabel>Password</TextFieldLabel>
+          <TextFieldInput type="password" placeholder="●●●●●●●●●" />
+        </TextField>
+        <Button>Log in to account</Button>
+      </CardContent>
+    </Card>
   );
 }
