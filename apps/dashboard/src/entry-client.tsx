089f3977cbaf1752acaf9685408c738aeb032909
chomnr <hello@zeljko.me>
2025-09-22 16:12:01 -0400

    removed nested to setup client

--- apps/dashboard/src/entry-client.tsx
+++ apps/dashboard/src/entry-client.tsx
@@ -1,11 +1,8 @@
 // @refresh reload
 import { setupClient } from '@safeoutput/lib/client/env/runtime';
-import { clientEnvSchema } from '@safeoutput/lib/client/env/schema';
 import { mount, StartClient } from '@solidjs/start/client';
 
 // inject client side envs into @safeoutput/lib
-setupClient({
-  client: clientEnvSchema.parse(import.meta.env),
-});
+setupClient(import.meta.env);
 
 mount(() => <StartClient />, document.getElementById('app')!);
