c63d0d5ad0deeefbc3fa4448793677bac2a2dcf0
chomnr <hello@zeljko.me>
2025-09-21 23:13:33 -0400

    made VITE_ORY_SDK_URL optional

--- packages/lib/src/client/env/schema.ts
+++ packages/lib/src/client/env/schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const clientEnvSchema = z.object({
   VITE_ROOT_DOMAIN: z.url(),
-  VITE_ORY_SDK_URL: z.url(),
+  VITE_ORY_SDK_URL: z.url().optional(),
   VITE_KRATOS_PUBLIC_URL: z.url().optional(),
   VITE_KRATOS_BROWSER_URL: z.url().optional(),
   VITE_KRATOS_ADMIN_URL: z.url().optional(),
