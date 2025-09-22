ec0aa0fbd60cb8c2e4d10de4f765ac4c0507cf35
chomnr <hello@zeljko.me>
2025-09-22 16:01:43 -0400

    testing ui

--- apps/dashboard/src/routes/(auth)/login.tsx
+++ apps/dashboard/src/routes/(auth)/login.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@safeoutput/ui/components/button';
+
 export default function Login() {
-  return <>dasds</>;
+  return (
+    <>
+      <Button>Log in to your account</Button>
+    </>
+  );
 }
