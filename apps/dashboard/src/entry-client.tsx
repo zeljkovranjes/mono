50d8f79bfdd6455f33a2c43e5c64b08fc57200c2
chomnr <hello@zeljko.me>
2025-09-21 20:43:04 -0400

    init the client for the @safeoutput/lib

--- apps/dashboard/src/entry-client.tsx
+++ apps/dashboard/src/entry-client.tsx
@@ -1,4 +1,11 @@
 // @refresh reload
+import { setupClient } from '@safeoutput/lib/client/env/runtime';
+import { clientEnvSchema } from '@safeoutput/lib/client/env/schema';
 import { mount, StartClient } from '@solidjs/start/client';
+
+// inject clientside envs into @safeoutput/lib
+setupClient({
+  client: clientEnvSchema.parse(import.meta.env),
+});
 
 mount(() => <StartClient />, document.getElementById('app')!);
