7a421cccfe2599ba9a3c8039b489d33853708fe5
chomnr <hello@zeljko.me>
2025-09-22 16:50:13 -0400

    shrunk height of lg button

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -28,7 +28,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-9.5 px-4 py-2',
         sm: 'h-8.5 px-3 text-xs',
-        lg: 'h-11 px-8',
+        lg: 'h-10.5 px-8',
         icon: 'size-10',
       },
     },
