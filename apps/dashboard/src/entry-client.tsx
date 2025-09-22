b47c4a9590d274a22ed655bfe3bfa056d638b5f2
chomnr <hello@zeljko.me>
2025-09-21 20:44:39 -0400

    fixed message

--- apps/dashboard/src/entry-client.tsx
+++ apps/dashboard/src/entry-client.tsx
@@ -3,7 +3,7 @@ import { setupClient } from '@safeoutput/lib/client/env/runtime';
 import { clientEnvSchema } from '@safeoutput/lib/client/env/schema';
 import { mount, StartClient } from '@solidjs/start/client';
 
-// inject clientside envs into @safeoutput/lib
+// inject client side envs into @safeoutput/lib
 setupClient({
   client: clientEnvSchema.parse(import.meta.env),
 });
