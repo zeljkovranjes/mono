92d01c0f2401f9073ecbe1e23011d98af2824ec8
chomnr <hello@zeljko.me>
2025-09-22 17:29:50 -0400

    made borders less round, lessen the shadow

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -7,7 +7,7 @@ const Card: Component<ComponentProps<'div'>> = (props) => {
   const [local, others] = splitProps(props, ['class']);
   return (
     <div
-      class={cn('rounded-lg border bg-card text-card-foreground shadow-sm', local.class)}
+      class={cn('rounded-md border bg-card text-card-foreground shadow-xs', local.class)}
       {...others}
     />
   );
@@ -15,13 +15,13 @@ const Card: Component<ComponentProps<'div'>> = (props) => {
 
 const CardHeader: Component<ComponentProps<'div'>> = (props) => {
   const [local, others] = splitProps(props, ['class']);
-  return <div class={cn('flex flex-col space-y-1.5 p-6', local.class)} {...others} />;
+  return <div class={cn('flex flex-col space-y-1.5 p-5', local.class)} {...others} />;
 };
 
 const CardTitle: Component<ComponentProps<'h3'>> = (props) => {
   const [local, others] = splitProps(props, ['class']);
   return (
-    <h3 class={cn('text-lg font-semibold leading-none tracking-tight', local.class)} {...others} />
+    <h3 class={cn('text-md font-medium leading-none tracking-tight', local.class)} {...others} />
   );
 };
 
@@ -32,12 +32,12 @@ const CardDescription: Component<ComponentProps<'p'>> = (props) => {
 
 const CardContent: Component<ComponentProps<'div'>> = (props) => {
   const [local, others] = splitProps(props, ['class']);
-  return <div class={cn('p-6 pt-0', local.class)} {...others} />;
+  return <div class={cn('p-5', local.class)} {...others} />;
 };
 
 const CardFooter: Component<ComponentProps<'div'>> = (props) => {
   const [local, others] = splitProps(props, ['class']);
-  return <div class={cn('flex items-center p-6 pt-0', local.class)} {...others} />;
+  return <div class={cn('flex items-center p-5', local.class)} {...others} />;
 };
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
