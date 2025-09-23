17865322ca8d14e24be6f94c4567992f70247746
chomnr <hello@zeljko.me>
2025-09-23 16:57:26 -0400

    setup package for payment-api

---
+++ apps/payment-api/src/server.ts
@@ -0,0 +1,25 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', async function handler(request, reply) {
+  return { some_variable: 'some value of variable' };
+});
+
+fastify.get('/about', async function handler(request, reply) {
+  return { info: 'Super puper information is saved here' };
+});
+
+async function start() {
+  try {
+    await fastify.listen({ port: 3000 });
+    console.log('Server is running on http://localhost:3000');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
