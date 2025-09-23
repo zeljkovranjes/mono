9071f9773e7343ef6a255db64b2b803099e1c758
chomnr <hello@zeljko.me>
2025-09-23 19:48:36 -0400

    testing server fastify

---
+++ apps/billing-api/src/server.ts
@@ -0,0 +1,36 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+  logger: true,
+  ajv: {
+    customOptions: {
+      coerceTypes: 'array',
+      removeAdditional: 'all',
+    },
+  },
+});
+
+async function init() {}
+
+init();
+/*
+fastify.get('/', async function handler(request, reply) {
+  return { some_variable: 'some value of variable' };
+});
+
+fastify.get('/about', async function handler(request, reply) {
+  return { info: 'Super puper information is saved here' };
+});
+
+async function start() {
+  try {
+    await fastify.listen({ port: 3009 });
+    console.log('Server is running on http://localhost:3009');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
+*/
