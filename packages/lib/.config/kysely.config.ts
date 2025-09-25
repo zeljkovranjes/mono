9724fd2468cfd7e85661a3ffa834f4f56e935c99
chomnr <hello@zeljko.me>
2025-09-25 17:11:13 -0400

    updated config with dialect

--- packages/lib/.config/kysely.config.ts
+++ packages/lib/.config/kysely.config.ts
@@ -1,32 +1,20 @@
 import {
-	DummyDriver,
-	PostgresAdapter,
-	PostgresIntrospector,
-	PostgresQueryCompiler,
-} from 'kysely'
-import { defineConfig } from 'kysely-ctl'
+  DummyDriver,
+  PostgresAdapter,
+  PostgresDialect,
+  PostgresIntrospector,
+  PostgresQueryCompiler,
+} from 'kysely';
+import { defineConfig } from 'kysely-ctl';
+import { postgresDialect } from '../src/server/db/pg';
 
 export default defineConfig({
-	// replace me with a real dialect instance OR a dialect name + `dialectConfig` prop.
-	dialect: {
-		createAdapter() {
-			return new PostgresAdapter()
-		},
-		createDriver() {
-			return new DummyDriver()
-		},
-		createIntrospector(db) {
-			return new PostgresIntrospector(db)
-		},
-		createQueryCompiler() {
-			return new PostgresQueryCompiler()
-		},
-	},
-	//   migrations: {
-	//     migrationFolder: "migrations",
-	//   },
-	//   plugins: [],
-	//   seeds: {
-	//     seedFolder: "seeds",
-	//   }
-})
+  dialect: postgresDialect,
+  //   migrations: {
+  //     migrationFolder: "migrations",
+  //   },
+  //   plugins: [],
+  //   seeds: {
+  //     seedFolder: "seeds",
+  //   }
+});
