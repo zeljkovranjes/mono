d655aa3df914a60a063e70141daef52f0a527bae
chomnr <hello@zeljko.me>
2025-09-26 19:58:43 -0400

    removed for_migrations

--- packages/lib/src/server/db/postgres/index.ts
+++ packages/lib/src/server/db/postgres/index.ts
@@ -9,23 +9,13 @@ import { DB } from '../types/pg-database-types';
  * - If `for_migrations` is `true`, then it manually runs the setupServerEnvironment
  * for when you run migrations.
  *
- * @param for_migratons - Whether this dialect is being created specifically
- *                        for running database migrations.
- *
  * @returns A `PostgresDialect` instance configured with a pooled connection.
  *
  * @example
- * // For migrations
- * const dialect = getPostgresDialect(true);
- *
- * // For normal runtime
- * const dialect = getPostgresDialect(false);
+ * const dialect = getPostgresDialect();
  */
-export function getPostgresDialect(for_migratons: boolean) {
-  if (for_migratons) {
-    setupServerEnvironment();
-  }
-
+export function getPostgresDialect() {
+  setupServerEnvironment();
   return new PostgresDialect({
     pool: new Pool({
       connectionString: getServerConfig().POSTGRES_DATABASE_URL,
@@ -34,7 +24,7 @@ export function getPostgresDialect(for_migratons: boolean) {
 }
 
 export const db = new Kysely<DB>({
-  dialect: getPostgresDialect(false),
+  dialect: getPostgresDialect(),
 });
 
 /*
