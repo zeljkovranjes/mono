515696067874279ae12be2af804688f38806d210
chomnr <hello@zeljko.me>
2025-09-29 20:59:31 -0400

    adjusted config for test

--- packages/lib/vitest.config.ts
+++ packages/lib/vitest.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    isolate: true,
+    clearMocks: true,
+    restoreMocks: true,
+    mockReset: true,
     environment: 'node',
     include: ['src/**/*.test.ts'],
     coverage: {
